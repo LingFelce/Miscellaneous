3234aa5e85e01a172a07f599d5f7150c8a88ae84
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-10-04 20:18:51 +0100

    test tracer assemble on 1 cell (not run yet)

--- flu_data_processing.sh
+++ flu_data_processing.sh
@@ -10,3 +10,8 @@ ls -1 *_1*.gz | awk -F '_' '{print $3}' | sort | uniq > ID
 for i in `cat ./ID`; do cat WTCHG_390445_$i\_1.fastq.gz WTCHG_392409_$i\_1.fastq.gz > merge/$i\_1.fastq.gz; done
 for i in `cat ./ID`; do cat WTCHG_390445_$i\_2.fastq.gz WTCHG_392409_$i\_2.fastq.gz > merge/$i\_2.fastq.gz; done
 
+# tracer test
+module load tracer
+Hsap = /databank/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta
+tracer assemble -p 8 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_files/merge/201249_2.fastq.gz P1_A1_UK001CD8A2_2006 test_output
+
