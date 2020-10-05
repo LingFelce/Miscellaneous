e1bc282b47dea32bb473275906fec65d66d2abcb
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-10-05 14:09:31 +0100

    new tracer assemble code seems to work with local tracer.conf file

--- flu_data_processing.sh
+++ flu_data_processing.sh
@@ -15,5 +15,21 @@ module load tracer
 Hsap = /databank/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta
 tracer assemble -p 8 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_files/merge/201249_2.fastq.gz P1_A1_UK001CD8A2_2006 test_output
 
-# found location of tracer.conf, can't seem to make edits though
+# found location of tracer.conf
 /package/tracer/implementing_build-20170413
+
+# copied tracer.conf into directory being used - maybe have to do for each experiment if paths are different?
+# output from running tracer assemble code: Config file not found at ~/.tracerrc. Using default tracer.conf in repo...
+
+# as a back up saved setup.py from git page as separate file in home folder and run - not sure if it'll work!
+
+# TraCeR looks for the configuration file, in descending order of priority, from the following sources:
+# The -c option used at run time for any of the TraCeR modes
+# The environmental variable TRACER_CONF, which can be set to point to a default location
+# The default global location of ~/.tracerrc
+# If all else fails, the provided example tracer.conf in the directory is used
+
+# already tried environmental variable and tracer.conf in directory used, can't find ~/.tracerrc
+
+# final option - added -c option (tracer.conf file in same folder as command being run)
+tracer assemble -c tracer.conf -p 8 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_files/merge/201249_2.fastq.gz P1_A1_UK001CD8A2_2006 test_output
