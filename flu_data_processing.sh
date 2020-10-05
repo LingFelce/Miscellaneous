0096cb7b2a70f57d24d51069c625aa96513c9697
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-10-05 14:29:06 +0100

    updated tracer assemble code to use more cores

--- flu_data_processing.sh
+++ flu_data_processing.sh
@@ -33,3 +33,6 @@ tracer assemble -p 8 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_f
 
 # final option - added -c option (tracer.conf file in same folder as command being run)
 tracer assemble -c tracer.conf -p 8 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_files/merge/201249_2.fastq.gz P1_A1_UK001CD8A2_2006 test_output
+
+# final bit with kallisto running really slowly, put more cores!
+tracer assemble -c tracer.conf -p 12 -s Hsap -q kallisto raw_files/merge/201249_1.fastq.gz raw_files/merge/201249_2.fastq.gz P1_A1_UK001CD8A2_2006 test_output
