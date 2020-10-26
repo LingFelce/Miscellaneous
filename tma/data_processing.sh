b0c20538f37c7c98e6ecdaa0b639db2e98a1d6cb
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-10-26 21:22:36 +0000

    merged files for 1st and 3rd run (within runs)

--- tma/data_processing.sh
+++ tma/data_processing.sh
@@ -10,3 +10,25 @@ pwd
 
 # type command into FASTQ folder (. indicates current folder)
 find . -type f -print0 | xargs -0 mv -t .
+
+# file name from NextSeq is different from file names generated from WTCHG sequencing
+TMA-181-R9_S47_L003_R1_001.fastq.gz
+# $1 is first variable in file name, $2 is second variable, $3 is third variable
+# $1 unique ID # $2 S_random number # $3 lane number # read number # all end in 001
+# $1 and $2 are the unique IDs combined!
+
+# find unique identifier/name for each sample and save in file called ID (should have 51 IDs per lane)
+ls -1 *_R1*.gz | awk -F '_' '{print $1"_"$2}' | sort | uniq > ID
+
+# merge files from lane 1 (L001), lane 2 (L002), lane 3 (L003) and lane 4 (L004) into a new folder called merge and save file name after unique ID. 
+# do separately for read 1 (_R1) and read 2 (_R2)
+for i in `cat ./ID`; do cat $i\_L001_R1_001.fastq.gz  $i\_L002_R1_001.fastq.gz  $i\_L003_R1_001.fastq.gz  $i\_L004_R1_001.fastq.gz > merge_201017/$i\_R1_001.fastq.gz; done
+for i in `cat ./ID`; do cat $i\_L001_R2_001.fastq.gz  $i\_L002_R2_001.fastq.gz  $i\_L003_R2_001.fastq.gz  $i\_L004_R2_001.fastq.gz > merge_201017/$i\_R2_001.fastq.gz; done
+
+# repeat for 3rd run 22th October (ran 1st pool again)
+for i in `cat ./ID`; do cat $i\_L001_R1_001.fastq.gz  $i\_L002_R1_001.fastq.gz  $i\_L003_R1_001.fastq.gz  $i\_L004_R1_001.fastq.gz > merge_201022/$i\_R1_001.fastq.gz; done
+for i in `cat ./ID`; do cat $i\_L001_R2_001.fastq.gz  $i\_L002_R2_001.fastq.gz  $i\_L003_R2_001.fastq.gz  $i\_L004_R2_001.fastq.gz > merge_201022/$i\_R2_001.fastq.gz; done
+
+# repeat for 2nd run 20th October (2nd pool only has 47 samples)
+# for some reason find command not working any more, keeps saying missing file operand?
+
