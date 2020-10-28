2512e653823b917f7e60ee9b2f178965af2defa0
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-10-28 22:47:11 +0000

    will need to zip downsampled files

--- tma/data_processing.sh
+++ tma/data_processing.sh
@@ -80,6 +80,8 @@ do
   seqtk sample -s100 $fq 20000000 > downsample/$name\.fastq
 done
 
+# downsample files are .fastq, need to zip
+gzip *fastq
 
 # soft links to src folder (do in folder that you want to move files to)
 # find /t1-data/user/ypeng/P170335/downloaded_files/lane1 -name "*.fastq.gz" | xargs -I v_f ln -s v_f
