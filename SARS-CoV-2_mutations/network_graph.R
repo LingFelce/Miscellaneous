ca6738450b73e10acb804ef85d4b46afc717e710
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-04-08 18:29:55 +0100

    ORF3a mutation network graph

--- SARS-CoV-2_mutations/network_graph.R
+++ SARS-CoV-2_mutations/network_graph.R
@@ -1,6 +1,4 @@
-#### Phylogenetic tree ####
-
-setwd("/t1-data/user/lfelce/R/")
+#### Network graphs ####
 
 library(tidyverse)
 library(ggraph)
@@ -10,15 +8,18 @@ library(igraph)
 
 # ORF3a replacement V202L
 
-metadata <- read.csv("/t1-data/user/lfelce/R/ORF3a_replacement_V202L.csv")
+metadata <- read.csv("/t1-data/user/lfelce/R/SARS-CoV-2_mutations/ORF3a_replacement_V202L.csv")
 
-df <- metadata[,c("virusName", "gisaidID", "covGlueLineage", "countryCode", "m49SubRegion")]
+df <- metadata[,c("virusName", "gisaidID", "covGlueLineage", "location", "countryCode", "m49SubRegion")]
+df$location <- gsub(x = df$location, pattern = " / ", replacement = "/")
+split <- str_split_fixed(df$location, "/", 3)
+df <- cbind(df, split)
 
 df2 <- df[,c("gisaidID", "covGlueLineage")]
 colnames(df2) <- c("to", "from")
 df2 <- df2[,c("from", "to")]
 
-df3 <- df[,c("gisaidID", "countryCode", "m49SubRegion")]
+df3 <- df[,c("gisaidID", "countryCode", "1")]
 colnames(df3) <- c("name", "country", "region")
 
 df4 <- as.data.frame(df[,c("covGlueLineage")])
@@ -30,31 +31,17 @@ df4$region <- "NA"
 
 df3 <- rbind(df3, df4)
 
-
 graph <- graph_from_data_frame(df2, vertices = df3)
-
-set.seed(1)
-ggraph(graph, 'circlepack') + 
-  geom_edge_link(aes(start_cap = label_rect(node1.name),
-                     end_cap = label_rect(node2.name))) +
-  geom_node_label(aes(label = name), repel=TRUE, label.size=0.1) +
-  geom_node_point(aes(fill=country))
 
 
 set.seed(1)
 ggraph(graph, 'circlepack') + 
   geom_edge_link() + 
   geom_node_point(aes(colour=region)) + 
-  geom_node_label(aes(label = ifelse(grepl("B", name), as.character(name), NA_character_)), repel=TRUE, label.size=0.25) +
+  geom_node_label(aes(label = ifelse(grepl("B", name), as.character(name), NA_character_)), repel=TRUE, label.size=0.1, size=6) +
   coord_fixed() +
   theme(legend.position = "bottom") +
   ggtitle("ORF3a replacement V202L")
-  
-
-
-
- 
-
 
 
 
