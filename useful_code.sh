9d8734f8544d39314eaa4313cf48049c7af8fb14
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-11-26 15:36:21 +0000

    renaming part of file name for multiple files in linux

--- useful_code.sh
+++ useful_code.sh
@@ -36,7 +36,38 @@ kill <PID>
 
 # move multiple files out of multiple subfolders and into main folder
 find . -type f -print0 | xargs -0 mv -t .
+
 # re-ordering columns of dataframe based on character
 test <- data.frame(a = c(1, 2, 3), b = c(4, 5, 6), c = c(7, 8, 9))
 cols <- c("b", "a", "c")
 test <- test[,cols]
+
+# renaming multiple files with common string in file name
+for i in s2_GEX-AK3472*; do mv ${i} ${i/#s2_GEX-AK3472/s2_GEX-1}; done
+for i in s2_GEX-AK3473*; do mv ${i} ${i/#s2_GEX-AK3473/s2_GEX-2}; done
+for i in s2_GEX-AK3474*; do mv ${i} ${i/#s2_GEX-AK3474/s2_GEX-3}; done
+for i in s2_GEX-AK3475*; do mv ${i} ${i/#s2_GEX-AK3475/s2_GEX-4}; done
+
+for i in s2_totalseq-AK2436*; do mv ${i} ${i/#s2_totalseq-AK2436/s2_totalseq-1}; done
+for i in s2_totalseq-AK8028*; do mv ${i} ${i/#s2_totalseq-AK8028/s2_totalseq-2}; done
+for i in s2_totalseq-AK8029*; do mv ${i} ${i/#s2_totalseq-AK8029/s2_totalseq-3}; done
+for i in s2_totalseq-AK8030*; do mv ${i} ${i/#s2_totalseq-AK8030/s2_totalseq-4}; done
+
+for i in s2_GEX-1_HN2L5DSX2*; do mv ${i} ${i/#s2_GEX-1_HN2L5DSX2/s2_GEX-1_A}; done
+for i in s2_GEX-1_HNT5KDSX2*; do mv ${i} ${i/#s2_GEX-1_HNT5KDSX2/s2_GEX-1_B}; done
+for i in s2_GEX-2_HN2L5DSX2*; do mv ${i} ${i/#s2_GEX-2_HN2L5DSX2/s2_GEX-2_A}; done
+for i in s2_GEX-2_HNT5KDSX2*; do mv ${i} ${i/#s2_GEX-2_HNT5KDSX2/s2_GEX-2_B}; done
+for i in s2_GEX-3_HN2L5DSX2*; do mv ${i} ${i/#s2_GEX-3_HN2L5DSX2/s2_GEX-3_A}; done
+for i in s2_GEX-3_HNT5KDSX2*; do mv ${i} ${i/#s2_GEX-3_HNT5KDSX2/s2_GEX-3_B}; done
+for i in s2_GEX-4_HN2L5DSX2*; do mv ${i} ${i/#s2_GEX-4_HN2L5DSX2/s2_GEX-4_A}; done
+for i in s2_GEX-4_HNT5KDSX2*; do mv ${i} ${i/#s2_GEX-4_HNT5KDSX2/s2_GEX-4_B}; done
+
+for i in s2_totalseq-1_HN2L5DSX2*; do mv ${i} ${i/#s2_totalseq-1_HN2L5DSX2/s2_totalseq-1_A}; done
+for i in s2_totalseq-1_HNT5KDSX2*; do mv ${i} ${i/#s2_totalseq-1_HNT5KDSX2/s2_totalseq-1_B}; done
+for i in s2_totalseq-2_HN2L5DSX2*; do mv ${i} ${i/#s2_totalseq-2_HN2L5DSX2/s2_totalseq-2_A}; done
+for i in s2_totalseq-2_HNT5KDSX2*; do mv ${i} ${i/#s2_totalseq-2_HNT5KDSX2/s2_totalseq-2_B}; done
+for i in s2_totalseq-3_HN2L5DSX2*; do mv ${i} ${i/#s2_totalseq-3_HN2L5DSX2/s2_totalseq-3_A}; done
+for i in s2_totalseq-3_HNT5KDSX2*; do mv ${i} ${i/#s2_totalseq-3_HNT5KDSX2/s2_totalseq-3_B}; done
+for i in s2_totalseq-4_HN2L5DSX2*; do mv ${i} ${i/#s2_totalseq-4_HN2L5DSX2/s2_totalseq-4_A}; done
+for i in s2_totalseq-4_HNT5KDSX2*; do mv ${i} ${i/#s2_totalseq-4_HNT5KDSX2/s2_totalseq-4_B}; done
+
